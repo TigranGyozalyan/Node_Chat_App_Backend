605acd05412fff411abaa43c7e38d63776a9a878
Tigran Gyozalyan <you@example.com>
2020-03-23 16:54:03 +0400

    add typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app: express.Application = express();
+const port: number = 3000;
+
+app.get('/', (req, res): void => {
+    res.send('Hello World!')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
