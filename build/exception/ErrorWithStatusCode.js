0ba2c70ac974b0919fd525dedf8d30efbf0bded2
Tigran Gyozalyan <you@example.com>
2020-05-17 22:36:14 +0400

    remove build and idea fules

--- build/exception/ErrorWithStatusCode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ErrorWithStatusCode extends Error {
-    constructor(message, statusCode = 500) {
-        super(message);
-        this.statusCode = statusCode;
-    }
-}
-exports.default = ErrorWithStatusCode;
